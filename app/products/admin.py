568215067b11eb662f7b8d03aa5b558cec63abb5
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-20 11:34:28 +0300

    register Size, Color, ProductImage, Product models in admin panel

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
+from django.db.models import ImageField
 
-# Register your models here.
+from .models import Color, Size, ProductImage, Product
+
+
+class ProdcutImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        output = []
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            file_name = str(value)
+            output.append(
+                f'<a href="{image_url}" target="_blank">'
+                f'<img src="{image_url}" alt="{file_name}" width="300" height="300" '
+                f'style="object-fit: cover;"/> </a>')
+
+        output.append(super(AdminFileWidget, self).render(name, value, attrs, renderer))
+        return mark_safe(u"".join(output))
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    formfield_overrides = {
+        ImageField: {"widget": ProdcutImageWidget}
+    }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "available", "quantity", "color", "size")
+    list_filter = ("available", "color", "size")
+    list_display_links = ("name", "color", "size")
+    inlines = [ProductImageInline]
+    readonly_fields = ["code"]
+    search_fields = ["name", "code"]
