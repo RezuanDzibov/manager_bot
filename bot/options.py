4def36b541bb6dbc47cc3aa9273cb9dc59945641
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-27 15:32:09 +0300

    add COMMIT_SOLD options

--- bot/options.py
+++ bot/options.py
@@ -1,4 +1,5 @@
 SEARCH_PRODUCT = "Найти товар"
 ADD_PRODUCT = "Добавить товар"
+COMMIT_SOLD = "Сообщить о продаже"
 
-__all__ = ["SEARCH_PRODUCT", "ADD_PRODUCT"]
+__all__ = ["SEARCH_PRODUCT", "ADD_PRODUCT", "COMMIT_SOLD"]
