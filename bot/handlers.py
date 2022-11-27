e58fdd7c4a809807c582a11961962598ea029180
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-27 15:34:53 +0300

    add COMMIT_SOLD option

--- bot/handlers.py
+++ bot/handlers.py
@@ -9,12 +9,13 @@ import states
 import services
 import markdown
 from settings import dp, bot
+import translates
 
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
-    markup.add(options.SEARCH_PRODUCT)
+    markup.add(options.SEARCH_PRODUCT, options.COMMIT_SOLD)
     await states.StartState.choice.set()
     await message.reply("Выбирете действие", reply_markup=markup)
 
@@ -38,17 +39,15 @@ async def process_search_product(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(lambda message: len(message.text) != 7, state=states.SearchState.code)
-async def process_code_invalid(message: types.Message):
-    return await message.reply(
-        "Артикул должен состоять из 7 символов, букв латинского алфавита и цифр. Пример: 11DC5A1 или 11dc5a1"
-    )
+async def process_search_product_code_invalid(message: types.Message):
+    return await message.reply(translates.CODE_INVALID_ANSWER)
 
 
 @dp.message_handler(state=states.SearchState)
 async def process_search_by_code(message: types.Message, state: FSMContext):
     product = await services.get_product(code=message.text.upper())
     if not product:
-        await bot.send_message(message.chat.id, f"Такого товара с артикулом: {message.text} не найдено.")
+        await bot.send_message(message.chat.id, translates.PRODUCT_NOT_FOUND.substitute(code=message.text))
         await state.finish()
         return
     if "images" in product:
@@ -63,4 +62,76 @@ async def process_search_by_code(message: types.Message, state: FSMContext):
     return
 
 
-__all__ = ["cmd_start", "cancel_handler", "process_search_product", "process_search_by_code"]
+@dp.message_handler(Text(contains=options.COMMIT_SOLD), state=states.StartState)
+async def process_commit_sold(message: types.Message, state: FSMContext):
+    await state.finish()
+    await states.SoldCommitState.code.set()
+    await message.reply("Введите артикул товара")
+
+
+@dp.message_handler(lambda message: len(message.text) != 7, state=states.SoldCommitState.code)
+async def process_sold_commit_code_invalid(message: types.Message):
+    return await message.reply(translates.CODE_INVALID_ANSWER)
+
+
+@dp.message_handler(state=states.SoldCommitState.code)
+async def process_sold_commit_code(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["code"] = message.text
+        await states.SoldCommitState.next()
+        await message.reply("Введите размер")
+
+
+@dp.message_handler(
+    lambda message: not message.text.isdigit() or int(message.text) < 1,
+    state=states.SoldCommitState.size
+)
+async def process_sold_commit_size_invalid(message: types.Message):
+    return await message.reply(
+        "Размер должно быть числом и не меньше 0"
+    )
+
+
+@dp.message_handler(state=states.SoldCommitState.size)
+async def process_sold_commit_size(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["size"] = message.text
+        await states.SoldCommitState.next()
+        await message.reply("Введите количество")
+
+
+@dp.message_handler(
+    lambda message: not message.text.isdigit() or int(message.text) < 1,
+    state=states.SoldCommitState.quantity
+)
+async def process_sold_commit_quantity_invalid(message: types.Message):
+    return await message.reply(
+        "Количество должно быть числом и не меньше 0"
+    )
+
+
+@dp.message_handler(state=states.SoldCommitState.quantity)
+async def process_sold_commit_quantity(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["quantity"] = message.text
+        await state.finish()
+        product = await services.commit_sold(*data.values())
+        if isinstance(product, dict):
+            product = await markdown.format_product_data(data=product)
+        await bot.send_message(message.chat.id, product)
+
+
+__all__ = [
+    "cmd_start",
+    "cancel_handler",
+    "process_search_product",
+    "process_search_product_code_invalid",
+    "process_search_by_code",
+    "process_commit_sold",
+    "process_sold_commit_code_invalid",
+    "process_sold_commit_code",
+    "process_sold_commit_size",
+    "process_sold_commit_quantity",
+    "process_sold_commit_size_invalid",
+    "process_sold_commit_quantity_invalid",
+]
