974df193b4d31237c1dfcb0ecccfbabdb6b46937
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-27 15:29:35 +0300

    add commit_sold endpoint

--- web/products/urls.py
+++ web/products/urls.py
@@ -7,7 +7,8 @@ router = DefaultRouter()
 router.register("", views.ProductViewSet, basename="product")
 
 urlpatterns = [
-    path("image/<int:pk>/", views.ProductImageRetrieve.as_view(), name="product_image")
+    path("image/<int:pk>/", views.ProductImageRetrieve.as_view(), name="product_image"),
+    path("commit_sold/<str:product_code>/", views.ProuductSoldView.as_view(), name="commit_sold")
 ]
 
 urlpatterns += router.urls
