37ce290208d98c360ff4c54f79c4fec9b9cbf10f
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-01 17:42:43 +0300

    add SizeAddSerializer, ProductAddSerializer, ProductImageExtensionValidator, ProductImageAddFiled, ProductImageAddSerializer

--- web/products/serializers.py
+++ web/products/serializers.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from rest_framework import serializers
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
 
 from .models import Product, ProductImage, Size
 
@@ -50,3 +54,70 @@ class ProductImageSerializer(serializers.ModelSerializer):
 class SoldCommitSerializer(serializers.Serializer):
     size = serializers.IntegerField(min_value=1)
     quantity = serializers.IntegerField(min_value=1)
+
+
+class SizeAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        exclude = ["product"]
+
+
+class ProductAddSerializer(serializers.ModelSerializer):
+    sizes = SizeAddSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "color",
+            "quantity",
+            "pack_quantity",
+            "wholesale_price",
+            "retail_price",
+            "supply_date",
+            "sold",
+            "remainder",
+            "defective",
+            "refund",
+            "sizes",
+        ]
+
+
+class ProductImageExtensionValidator(FileExtensionValidator):
+    def __call__(self, value):
+        extension = Path(value).suffix[1:].lower()
+        if (
+            self.allowed_extensions is not None
+            and extension not in self.allowed_extensions
+        ):
+            raise ValidationError(
+                self.message,
+                code=self.code,
+                params={
+                    "extension": extension,
+                    "allowed_extensions": ", ".join(self.allowed_extensions),
+                    "value": value,
+                },
+            )
+
+
+class ProductImageAddFiled(serializers.CharField):
+    default_error_messages = {
+        "invalid": "Not a valid image path.",
+        "blank": "This field may not be blank.",
+        "max_length": "Ensure this field has no more than {max_length} characters.",
+        "min_length": "Ensure this field has at least {min_length} characters.",
+        "image_extension": "Ensure this field has extension jpg or png"
+    }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.validators.append(ProductImageExtensionValidator(["png", "jpg"]))
+
+
+class ProductImageAddSerializer(serializers.Serializer):
+    images = serializers.ListSerializer(child=ProductImageAddFiled())
+
+    class Meta:
+        model = ProductImage
+        fields = ["images"]
