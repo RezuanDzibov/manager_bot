dad1e4a5691d68f13d3d29044cbfe1a0df149524
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-01 17:45:15 +0300

    add product_add and product_image_add urls

--- web/products/urls.py
+++ web/products/urls.py
@@ -1,5 +1,5 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
@@ -7,8 +7,10 @@ router = DefaultRouter()
 router.register("", views.ProductViewSet, basename="product")
 
 urlpatterns = [
+    path("image/<str:code>/", views.ProductImageAddView.as_view(), name="product_image_add"),
     path("image/<int:pk>/", views.ProductImageRetrieve.as_view(), name="product_image"),
-    path("commit_sold/<str:product_code>/", views.ProuductSoldView.as_view(), name="commit_sold")
+    path("commit_sold/<str:product_code>/", views.ProuductSoldView.as_view(), name="commit_sold"),
+    path("", views.ProuductCreateView.as_view(), name="product_add"),
 ]
 
 urlpatterns += router.urls
