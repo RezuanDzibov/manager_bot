89b102abb9e95c907ee7b71cc03c415c05d5a955
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-01 18:02:00 +0300

    add utils.py to handlers

---
+++ bot/handlers/utils.py
@@ -0,0 +1,17 @@
+from aiogram import types
+
+from settings import bot
+from . import md
+
+
+async def show_product(message: types.Message, product: dict):
+    if "images" in product:
+        await bot.send_media_group(message.chat.id, media=product.pop("images"))
+    product = await md.format_product_data(data=product)
+    await bot.send_message(
+        message.chat.id,
+        product,
+        parse_mode=types.ParseMode.MARKDOWN,
+    )
+    return
+
