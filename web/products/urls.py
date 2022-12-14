ea725b1cdb529eecb2e5f4184419fa64fbf17b17
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-14 16:11:32 +0300

    remove product & image add endpoints

--- web/products/urls.py
+++ web/products/urls.py
@@ -7,10 +7,8 @@ router = DefaultRouter()
 router.register("", views.ProductViewSet, basename="product")
 
 urlpatterns = [
-    path("image/<str:code>/", views.ProductImageAddView.as_view(), name="product_image_add"),
     path("image/<int:pk>/", views.ProductImageRetrieve.as_view(), name="product_image"),
-    path("commit_sold/<str:product_code>/", views.ProuductSoldView.as_view(), name="commit_sold"),
-    path("", views.ProductCreateView.as_view(), name="product_add"),
+    path("commit_sold/<str:product_code>/", views.ProductSoldView.as_view(), name="commit_sold"),
 ]
 
 urlpatterns += router.urls
--- web/products/views.py
+++ web/products/views.py
@@ -2,18 +2,17 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import RetrieveAPIView, GenericAPIView
 from rest_framework.request import HttpRequest
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from . import services
 from .models import Product, ProductImage
-from .serializers import ProductSerializer, ProductImageSerializer, SoldCommitSerializer, ProductAddSerializer
+from .serializers import ProductSerializer, ProductImageSerializer, SoldCommitSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    http_method_names = ["get", "head", "patch", "delete"]
+    http_method_names = ["get", "head", "patch"]
     lookup_field = "code"
 
 
@@ -22,7 +21,7 @@ class ProductImageRetrieve(RetrieveAPIView):
     serializer_class = ProductImageSerializer
 
 
-class ProuductSoldView(GenericAPIView):
+class ProductSoldView(GenericAPIView):
     serializer_class = SoldCommitSerializer
 
     @swagger_auto_schema()
@@ -33,19 +32,3 @@ class ProuductSoldView(GenericAPIView):
             quantity=int(request.data["quantity"])
         )
         return Response(status=200, data=sold)
-
-
-class ProductCreateView(GenericAPIView):
-    serializer_class = ProductAddSerializer
-
-    @swagger_auto_schema(responses={201: ProductSerializer()})
-    def post(self, request: HttpRequest) -> Response:
-        product = services.create_product(data=request.data.copy())
-        return Response(status=201, data=ProductSerializer(product).data)
-
-
-class ProductImageAddView(APIView):
-    @swagger_auto_schema()
-    def post(self, request: HttpRequest, code: str) -> Response:
-        services.add_image_to_product(product_code=code, data=request.data.copy())
-        return Response(status=201)
