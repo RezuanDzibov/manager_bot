5b9a7ef3b8258d700889e61ac2fefe669138a57b
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-28 20:41:00 +0300

    try to fix static

--- web/config/settings.py
+++ web/config/settings.py
@@ -130,9 +130,7 @@ USE_TZ = True
 
 STATIC_URL = os.environ.get("STATIC_URL")
 STATIC_ROOT = os.environ.get("STATIC_HOME") + "/"
-STATICFILES_DIRS = [
-    os.path.join("/var/www/", "staticfiles")
-]
+print(STATIC_URL, STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
