75cb2f874eabf5291e47f00368823b7f3ff9a4cf
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-29 21:51:58 +0300

    try to fix static

--- web/config/settings.py
+++ web/config/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.environ.get("STATIC_URL")
-STATIC_ROOT = os.environ.get("STATIC_HOME") + "/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR.parent / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = os.environ.get("MEDIA_URL")
-MEDIA_ROOT = os.environ.get("MEDIA_HOME") + "/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.parent / "mediafiles"
