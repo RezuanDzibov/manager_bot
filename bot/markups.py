948b638f288de5c9020b2acc78982d56afbf7d78
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-01-18 19:53:19 +0300

    add get_sizes_markup function

--- bot/markups.py
+++ bot/markups.py
@@ -7,3 +7,10 @@ async def get_start_markup() -> types.ReplyKeyboardMarkup:
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
     markup.add(translates.SEARCH_PRODUCT, translates.COMMIT_SOLD)
     return markup
+
+
+async def get_sizes_markup(sizes: list) -> types.ReplyKeyboardMarkup:
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True, row_width=1)
+    sizes = [types.KeyboardButton(f"Размер: {size[0]}\nКоличество: {size[1]}") for size in sizes]
+    markup.add(*sizes)
+    return markup
