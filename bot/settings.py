a0c523b085996d5a3f4ffa9eaf4e994ff9a80a1e
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-01-24 15:12:50 +0300

    use redis as storage

--- bot/settings.py
+++ bot/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import aiohttp
 from dotenv import load_dotenv, find_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -18,8 +18,10 @@ API_TOKEN = str(os.environ.get("BOT_API_TOKEN"))
 API_AUTH_USERNAME = str(os.environ.get("API_AUTH_USERNAME"))
 API_AUTH_PASSWORD = str(os.environ.get("API_AUTH_PASSWORD"))
 ALLOWED_USERS = [int(id_) for id_ in str(os.environ.get("ALLOWED_USERS")).split(" ")]
+REDIS_HOST = str(os.environ.get("REDIS_HOST"))
+REDIS_PORT = int(os.environ.get("REDIS_PORT"))
 
 bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
+storage = RedisStorage2(REDIS_HOST, REDIS_PORT)
 dp = Dispatcher(bot, storage=storage)
 AUTH = aiohttp.BasicAuth(login=API_AUTH_USERNAME, password=API_AUTH_PASSWORD)
