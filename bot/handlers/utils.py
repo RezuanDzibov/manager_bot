cfe050f77b2cf5f759acc0dfdae6745759eae288
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-01-24 15:11:15 +0300

    add send_start_markup & check_invalid_sizes

--- bot/handlers/utils.py
+++ bot/handlers/utils.py
@@ -1,8 +1,17 @@
 from aiogram import types
 
 import md
-from markups import get_cancel_markup
-from settings import bot
+import states
+from markups import get_cancel_markup, get_start_markup
+from settings import bot, dp
+
+
+async def send_start_markup(chat_id):
+    state = dp.current_state()
+    await state.finish()
+    markup = await get_start_markup()
+    await states.StartState.choice.set()
+    return await bot.send_message(chat_id, "Выберите действие", reply_markup=markup)
 
 
 async def show_product(message: types.Message, product: dict):
@@ -19,4 +28,12 @@ async def show_product(message: types.Message, product: dict):
 
 async def cancel(message: types.Message):
     cancel_markup = await get_cancel_markup()
-    await bot.send_message(message.from_id, "Отменить действие?", reply_markup=cancel_markup)
+    await bot.send_message(chat_id, "Отменить действие?", reply_markup=cancel_markup)
+
+
+async def check_invalid_sizes(sizes: list):
+    to_return = list()
+    for size, size_quantity in sizes:
+        if size_quantity == 0:
+            to_return.append(str(size))
+    return to_return
