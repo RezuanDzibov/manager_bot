d96c0b006418998f66ef128c7ed7839d3221f4cd
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-01-24 13:00:55 +0300

    register ProductSoldPackView in urls

--- web/products/urls.py
+++ web/products/urls.py
@@ -9,6 +9,7 @@ router.register("", views.ProductViewSet, basename="product")
 urlpatterns = [
     path("image/<int:pk>/", views.ProductImageRetrieve.as_view(), name="product_image"),
     path("commit_sold/<str:product_code>/", views.ProductSoldView.as_view(), name="commit_sold"),
+    path("commit_sold/pack/<str:product_code>/", views.ProductSoldPackView.as_view(), name="commit_sold_pack"),
 ]
 
 urlpatterns += router.urls
