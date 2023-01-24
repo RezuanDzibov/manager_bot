3e7ca29f65ef8f935332417bf9ca51bc95178a95
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-01-24 17:52:30 +0300

    try to fix

--- bot/handlers/base.py
+++ bot/handlers/base.py
@@ -4,10 +4,9 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext, Dispatcher
 from aiogram.dispatcher.filters import Text
 
-import states
 from filters import validate_user
-from markups import get_start_markup
-from settings import dp, bot
+from handlers.utils import send_start_markup
+from settings import dp
 
 
 async def on_shutdown(dispatcher: Dispatcher):
@@ -21,9 +20,7 @@ async def check_user_id(message: types.Message):
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    markup = await get_start_markup()
-    await states.StartState.choice.set()
-    await message.reply("Выберите действие", reply_markup=markup)
+    await send_start_markup(chat_id=message.chat.id)
 
 
 @dp.message_handler(state="*", commands="cancel")
@@ -31,23 +28,18 @@ async def cmd_start(message: types.Message):
 async def cancel_handler(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
-        return
+        return await send_start_markup(chat_id=message.chat.id)
     logging.info(f"Cancelling state {current_state}")
-    await state.finish()
-    markup = await get_start_markup()
-    await message.reply("Отмена действия", reply_markup=markup)
+    await send_start_markup(chat_id=message.chat.id)
 
 
 @dp.callback_query_handler(text="cancel", state="*")
 async def cancel_q(call: types.CallbackQuery, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
-        return
+        return await send_start_markup(chat_id=call.from_user.id)
     logging.info(f"Cancelling state {current_state}")
-    await state.finish()
-    await states.StartState.choice.set()
-    markup = await get_start_markup()
-    await bot.send_message(call.from_user.id, "Выберите действие", reply_markup=markup)
+    await send_start_markup(chat_id=call.from_user.id)
 
 
 __all__ = [
